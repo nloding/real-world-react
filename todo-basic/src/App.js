988818ff47eb477ba5e674caa12d74f8cc27cc6b
Nathan Loding <loding@gmail.com>
2018-01-05 16:14:31 -0500

    bring in bootstrap, fa, sass

--- todo-basic/src/App.js
+++ todo-basic/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="alert alert-success">TEST</div>
       </div>
     );
   }
